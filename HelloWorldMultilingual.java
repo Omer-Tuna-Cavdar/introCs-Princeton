184506d5e3b17089e8fc0eee776e02840505e0ac
Omer Tuna Çavdar <143618537+Omer-Tuna-Cavdar@users.noreply.github.com>
2023-09-06 09:23:21 +0000

    Web-Exercise-1 1.1

---
+++ HelloWorldMultilingual.java
@@ -0,0 +1,15 @@
+public class HelloWorldMultilingual {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Bonjour le monde!");
+        System.out.println("Hola Mundo!");
+        System.out.println("Hallo Welt!");
+        System.out.println("Ciao Mondo!");
+        System.out.println("Hej varlden!");
+        System.out.println("Ahoj svete!");
+        System.out.println("Pozdrav svijete!");
+        System.out.println("Привет мир!");
+        System.out.println("Γειά σου Κόσμε!");  
+    }
+    
+}
