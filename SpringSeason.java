29d6174f4a6eaa4db3a50a3caff4fb1f6440912a
Omer Tuna Çavdar <143618537+Omer-Tuna-Cavdar@users.noreply.github.com>
2023-09-08 09:59:31 +0000

    Forgot to close Scanner

--- SpringSeason.java
+++ SpringSeason.java
@@ -8,6 +8,7 @@ public class SpringSeason {
         int month = keyboard.nextInt();
         System.out.println("Enter day: ");
         int day = keyboard.nextInt();
+        keyboard.close();
         if(month == 3 && day >= 20 && day <= 31 || month == 4 && day >= 1 && day <= 30 || month == 5 && day >= 1 && day <= 31 || month == 6 && day >= 1 && day <= 20) {
             System.out.println("It's spring!");
         } else {
