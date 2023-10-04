b4573fe112e7fb8cf0b711aea0dd2b95a3229608
Omer Tuna Çavdar <143618537+Omer-Tuna-Cavdar@users.noreply.github.com>
2023-10-04 20:37:10 +0000

    Web Exercises 1

---
+++ 1.3-Conditionals-and-Loops/RollFairDice.java
@@ -0,0 +1,6 @@
+public class RollFairDice {
+    public static void main(String[] args) {
+        int roll = (int) (Math.random() * 6) + 1;
+        System.out.println("You rolled a " + roll + "!");
+    }
+}
